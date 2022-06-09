bff63492612de48ee84624081b792b0505e32231
Coffee173 <luanova173@gmail.com>
2022-06-09 16:35:45 -0300

    First Commit

---
+++ PowerPoint/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace PowerPoint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.novoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirTextoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvarTextoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copiarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opçõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.negritoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.itálicoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sublinhadoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.alterarFonteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.alinharTextoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.esquerdaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.direitaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.centroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.apresentarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PowerPoint.Properties.Resources.frases_do_coringa_2;
+            this.pictureBox1.Location = new System.Drawing.Point(69, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.opçõesToolStripMenuItem,
+            this.apresentarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(988, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.novoToolStripMenuItem,
+            this.abrirTextoToolStripMenuItem,
+            this.salvarTextoToolStripMenuItem,
+            this.copiarToolStripMenuItem,
+            this.colarToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // novoToolStripMenuItem
+            // 
+            this.novoToolStripMenuItem.Name = "novoToolStripMenuItem";
+            this.novoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.novoToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.novoToolStripMenuItem.Text = "Novo";
+            this.novoToolStripMenuItem.Click += new System.EventHandler(this.novoToolStripMenuItem_Click);
+            // 
+            // abrirTextoToolStripMenuItem
+            // 
+            this.abrirTextoToolStripMenuItem.Name = "abrirTextoToolStripMenuItem";
+            this.abrirTextoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.abrirTextoToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.abrirTextoToolStripMenuItem.Text = "Abrir Texto";
+            this.abrirTextoToolStripMenuItem.Click += new System.EventHandler(this.abrirTextoToolStripMenuItem_Click);
+            // 
+            // salvarTextoToolStripMenuItem
+            // 
+            this.salvarTextoToolStripMenuItem.Name = "salvarTextoToolStripMenuItem";
+            this.salvarTextoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.salvarTextoToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.salvarTextoToolStripMenuItem.Text = "Salvar Texto";
+            this.salvarTextoToolStripMenuItem.Click += new System.EventHandler(this.salvarTextoToolStripMenuItem_Click);
+            // 
+            // copiarToolStripMenuItem
+            // 
+            this.copiarToolStripMenuItem.Name = "copiarToolStripMenuItem";
+            this.copiarToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.copiarToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.copiarToolStripMenuItem.Text = "Copiar";
+            // 
+            // colarToolStripMenuItem
+            // 
+            this.colarToolStripMenuItem.Name = "colarToolStripMenuItem";
+            this.colarToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.colarToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.colarToolStripMenuItem.Text = "Colar";
+            // 
+            // opçõesToolStripMenuItem
+            // 
+            this.opçõesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.negritoToolStripMenuItem,
+            this.itálicoToolStripMenuItem,
+            this.sublinhadoToolStripMenuItem,
+            this.alterarFonteToolStripMenuItem,
+            this.alinharTextoToolStripMenuItem});
+            this.opçõesToolStripMenuItem.Name = "opçõesToolStripMenuItem";
+            this.opçõesToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.opçõesToolStripMenuItem.Text = "Opções";
+            // 
+            // negritoToolStripMenuItem
+            // 
+            this.negritoToolStripMenuItem.Name = "negritoToolStripMenuItem";
+            this.negritoToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.negritoToolStripMenuItem.Text = "Negrito";
+            // 
+            // itálicoToolStripMenuItem
+            // 
+            this.itálicoToolStripMenuItem.Name = "itálicoToolStripMenuItem";
+            this.itálicoToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.itálicoToolStripMenuItem.Text = "Itálico";
+            // 
+            // sublinhadoToolStripMenuItem
+            // 
+            this.sublinhadoToolStripMenuItem.Name = "sublinhadoToolStripMenuItem";
+            this.sublinhadoToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.sublinhadoToolStripMenuItem.Text = "Sublinhado";
+            // 
+            // alterarFonteToolStripMenuItem
+            // 
+            this.alterarFonteToolStripMenuItem.Name = "alterarFonteToolStripMenuItem";
+            this.alterarFonteToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.alterarFonteToolStripMenuItem.Text = "Alterar Fonte";
+            // 
+            // alinharTextoToolStripMenuItem
+            // 
+            this.alinharTextoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.esquerdaToolStripMenuItem,
+            this.direitaToolStripMenuItem,
+            this.centroToolStripMenuItem});
+            this.alinharTextoToolStripMenuItem.Name = "alinharTextoToolStripMenuItem";
+            this.alinharTextoToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.alinharTextoToolStripMenuItem.Text = "Alinhar Texto";
+            // 
+            // esquerdaToolStripMenuItem
+            // 
+            this.esquerdaToolStripMenuItem.Name = "esquerdaToolStripMenuItem";
+            this.esquerdaToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.esquerdaToolStripMenuItem.Text = "Esquerda";
+            // 
+            // direitaToolStripMenuItem
+            // 
+            this.direitaToolStripMenuItem.Name = "direitaToolStripMenuItem";
+            this.direitaToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.direitaToolStripMenuItem.Text = "Direita";
+            // 
+            // centroToolStripMenuItem
+            // 
+            this.centroToolStripMenuItem.Name = "centroToolStripMenuItem";
+            this.centroToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.centroToolStripMenuItem.Text = "Centro";
+            // 
+            // apresentarToolStripMenuItem
+            // 
+            this.apresentarToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("apresentarToolStripMenuItem.Image")));
+            this.apresentarToolStripMenuItem.Name = "apresentarToolStripMenuItem";
+            this.apresentarToolStripMenuItem.Size = new System.Drawing.Size(92, 20);
+            this.apresentarToolStripMenuItem.Text = "Apresentar";
+            this.apresentarToolStripMenuItem.Click += new System.EventHandler(this.apresentarToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(733, 262);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Pink;
+            this.ClientSize = new System.Drawing.Size(988, 519);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem novoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirTextoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvarTextoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copiarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opçõesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem negritoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem itálicoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sublinhadoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem alterarFonteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem alinharTextoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem esquerdaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem direitaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem centroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem apresentarToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ PowerPoint/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PowerPoint
+{
+    public partial class Form1 : Form
+    {
+        private bool Dragging;
+        private int xPos;
+        private int yPos;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Dragging = false;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Dragging = true;
+                xPos = e.X;
+                yPos = e.Y;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Control c = sender as Control;
+            if (Dragging && c != null)
+            {
+                c.Top = e.Y + c.Top - yPos;
+                c.Left = e.X + c.Left - xPos;
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void richTextBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Dragging = false;
+        }
+
+        private void richTextBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Dragging = true;
+                xPos = e.X;
+                yPos = e.Y;
+            }
+        }
+
+        private void richTextBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Control c = sender as Control;
+            if (Dragging && c != null)
+            {
+                c.Top = e.Y + c.Top - yPos;
+                c.Left = e.X + c.Left - xPos;
+            }
+        }
+
+        private void salvarTextoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "txt files (.txt)|.txt|All files (.)|.";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    // Code to write the stream goes here.
+                    myStream.Close();
+
+                }
+            }
+        }
+
+        private void apresentarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //cria codigo pra apresentar a imagem 
+
+
+        }
+
+        private void novoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void abrirTextoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Menu_Copy(System.Object sender, System.EventArgs e)
+        {
+            // Ensure that text is selected in the text box.   
+            if (textBox1.SelectionLength > 0)
+                // Copy the selected text to the Clipboard.
+                textBox1.Copy();
+        }
+
+        private void Menu_Cut(System.Object sender, System.EventArgs e)
+        {
+            // Ensure that text is currently selected in the text box.   
+            if (textBox1.SelectedText != "")
+                // Cut the selected text in the control and paste it into the Clipboard.
+                textBox1.Cut();
+        }
+
+        private void Menu_Paste(System.Object sender, System.EventArgs e)
+        {
+            // Determine if there is any text in the Clipboard to paste into the text box.
+            if (Clipboard.GetDataObject().GetDataPresent(DataFormats.Text) == true)
+            {
+                // Determine if any text is selected in the text box.
+                if (textBox1.SelectionLength > 0)
+                {
+                    // Ask user if they want to paste over currently selected text.
+                    if (MessageBox.Show("Do you want to paste over current selection?", "Cut Example", MessageBoxButtons.YesNo) == DialogResult.No)
+                        textBox1.SelectionStart = textBox1.SelectionStart + textBox1.SelectionLength;
+                }
+                // Paste current text in Clipboard into text box.
+                textBox1.Paste();
+            }
+        }
+    }
+}
